b084251bfcccd44ed4c6f116984892544d043b94
Tj <tj@ark.ph>
2023-07-27 12:41:27 +0800

    about first update

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/Layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>It's my first time to create website.</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>About Me</title>
+
+export default AboutPage
